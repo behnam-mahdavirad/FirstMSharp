51eb93ecab6cb7fc64775464c43acae12092163a
Behnam Mahdavirad <behnam.mahdavirad@gmail.com>
2020-08-13 11:18:16 +0430

    Added Contact Entity

--- M#/Model/Domain/Contact.cs
+++ M#/Model/Domain/Contact.cs
@@ -6,7 +6,11 @@ namespace Domain
     {
         public Contact()
         {
-
+            Associate<Category>("Category").Mandatory(); // Many to one
+            String("First name").Mandatory();
+            String("Last name").Mandatory();
+            String("Tel").Mandatory();
+            String("Email").Mandatory().Accepts(TextPattern.EmailAddress);
         }
     }
 }
