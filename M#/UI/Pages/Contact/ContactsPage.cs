ca2fca24afd47bb28ad8a000a74976c7529f5329
Behnam Mahdavirad <behnam.mahdavirad@gmail.com>
2020-08-13 11:48:44 +0430

    * Added ContactPage (RootPage) * Added Contactspage (SubPage) * Added ContactList Module

---
+++ M#/UI/Pages/Contact/ContactsPage.cs
@@ -0,0 +1,17 @@
+﻿using MSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UI.Pages.Contact
+{
+    class ContactsPage : SubPage<ContactPage>
+    {
+        public ContactsPage()
+        {
+            Layout(Layouts.AdminDefault);
+
+            Add<Modules.ContactsList>():
+        }
+    }
+}
---
+++ M#/UI/Pages/ContactPage.cs
@@ -0,0 +1,17 @@
+﻿using MSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UI.Pages
+{
+    public class ContactPage : RootPage
+    {
+        public ContactPage()
+        {
+            Add<Modules.MainMenu>();
+
+            OnStart(x => x.Go<Contact.ContactsPage>().RunServerSide());
+        }
+    }
+}
